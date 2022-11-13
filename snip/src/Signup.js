ad24bd9814785d5124204ba6bcc9eaea298b665b
Barine Desire <desirebarine16@gmail.com>
2022-11-13 22:39:46 +0100

    added signup functionality

--- snip/src/Signup.js
+++ snip/src/Signup.js
@@ -1,19 +1,35 @@
 import { Link } from 'react-router-dom'
-
-
+import { useState } from "react"
 const Signup = () =>{ 
+
+	const [name, setName] = useState("")
+	const [email, setEmail] = useState("")
+	const [password, setPassword] = useState("")
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		const user = {name, email, password}
+
+		fetch('http://localhost:5000/signup', {
+			method: 'POST',
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(user)
+		}).then(() => {
+			alert("new_user")
+		})
+	}
 	return (
 	<div className="Signup-page">
 		<h1>SS Snipstore</h1>
 		<h2>Create an account</h2>
-		<form>
+		<form onSubmit={handleSubmit}>
 		<h3>Name</h3>
-		<input/>
+		<input required className="signup" type="text" value={name} onChange={(e) => setName(e.target.value)}/>
 		<h3>Email</h3>
-		<input/>
+		<input required className="signup" type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
 		<h3>Password</h3>
-		<input/>
-		<button><Link to="/sign-in">Signup</Link></button>
+		<input required className="signup" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+		<button>Signup</button>
 		</form>
 	</div>
 	);
